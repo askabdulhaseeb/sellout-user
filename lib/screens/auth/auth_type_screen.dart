32bdeda6bf0f3fb76d100ab8413caaec21733a1a
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-01-05 01:51:31 +0500

    auth type screens UI complete.

--- lib/screens/auth/auth_type_screen.dart
+++ lib/screens/auth/auth_type_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sellout/utilities/app_images.dart';
+import 'package:sellout/utilities/utilities.dart';
+import 'package:sellout/widgets/custom_elevated_button.dart';
 
 class AuthTypeScreen extends StatelessWidget {
   const AuthTypeScreen({Key? key}) : super(key: key);
@@ -6,6 +9,38 @@ class AuthTypeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Padding(
+        padding: EdgeInsets.all(Utilities.padding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: Image.asset(AppImages.iconSimple),
+            ),
+            CustomElevatedButton(
+              title: 'Login',
+              onTap: () {},
+              bgColor: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              textStyle: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            CustomElevatedButton(
+              onTap: () {},
+              title: 'Register',
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utilities/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static String get iconBorder => 'assets/images/icon_border.png';
+  static String get iconSimple => 'assets/images/icon_simple.png';
+  static String get logo => 'assets/images/logo.png';
+}
