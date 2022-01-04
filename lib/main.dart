c9d351d545df6a2dd5c50b41a26d274f9e8949b8
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-01-05 00:45:30 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,27 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    const Color _kPrimaryColor = Color(0xFFD32F2F);
+    const Color _kSecondaryColor = Color(0xFF7C4DFF);
     return MaterialApp(
       title: 'SellOut',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: _kPrimaryColor,
+        colorScheme: const ColorScheme(
+          primary: _kPrimaryColor,
+          primaryVariant: Colors.red,
+          secondary: _kSecondaryColor,
+          secondaryVariant: Colors.deepPurple,
+          surface: Colors.white,
+          background: Colors.white,
+          error: Colors.red,
+          onPrimary: Colors.red,
+          onSecondary: Colors.deepPurple,
+          onSurface: Colors.grey,
+          onBackground: Colors.grey,
+          onError: Colors.red,
+          brightness: Brightness.light,
+        ),
       ),
       home: const AuthTypeScreen(),
       routes: <String, WidgetBuilder>{
