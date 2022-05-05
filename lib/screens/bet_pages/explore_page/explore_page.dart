f44cb21817a5fff8ccbd8c2617b0f6e2ad18346b
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-05-05 13:08:56 +0500

    Update explore_page.dart

--- lib/screens/bet_pages/explore_page/explore_page.dart
+++ lib/screens/bet_pages/explore_page/explore_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../enums/delivery_type.dart';
+import '../../../enums/product_condition.dart';
 import '../../../models/product.dart';
 import '../../../providers/prod_provider.dart';
 import '../../../services/user_local_data.dart';
@@ -32,8 +34,32 @@ class ExplorePage extends StatelessWidget {
             onChanged: (String? value) => _prodProvider.onSearch(value),
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
+              Expanded(
+                child: SizedBox(
+                  height: 36,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      if (_prodProvider.maxPrice >= _prodProvider.minPrice)
+                        _FilterTile(
+                          title:
+                              'Price ${_prodProvider.minPrice}-${_prodProvider.maxPrice}',
+                        ),
+                      if (_prodProvider.condition != null)
+                        _FilterTile(
+                          title:
+                              'Condition: ${ProdConditionEnumConvertor.enumToString(condition: _prodProvider.condition ?? ProdConditionEnum.NEW)}',
+                        ),
+                      if (_prodProvider.deliveryType != null)
+                        _FilterTile(
+                          title:
+                              'Delivery Type: ${DeliveryTypeEnumConvertor.enumToString(delivery: _prodProvider.deliveryType ?? DeliveryTypeEnum.BOTH)}',
+                        ),
+                    ],
+                  ),
+                ),
+              ),
               TextButton(
                 onPressed: () {
                   Navigator.of(context).pushNamed(ProdFilterScreen.routeName);
@@ -53,6 +79,28 @@ class ExplorePage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _FilterTile extends StatelessWidget {
+  const _FilterTile({required this.title, Key? key}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      margin: const EdgeInsets.only(right: 10),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Theme.of(context).primaryColor,
+        ),
+        borderRadius: BorderRadius.circular(24),
+      ),
+      alignment: Alignment.center,
+      child: Text(title),
     );
   }
 }
